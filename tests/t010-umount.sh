3ba86da114100e0bb4655b96e88a9f49ca2d350a
Ali Polatel <alip@exherbo.org>
2010-12-22 04:28:36 +0200

    Add skeleton tests

---
+++ tests/t010-umount.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# vim: set sw=4 et ts=4 sts=4 tw=80 :
+# Copyright 2010 Ali Polatel <alip@exherbo.org>
+# Distributed under the terms of the GNU General Public License v2
+
+test_description='sandbox umount(2)'
+. ./test-lib.sh
+
+test_done
