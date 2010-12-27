ec8a37d64516a506f8ed0946c5aa6c50c04c304d
Ali Polatel <alip@exherbo.org>
2010-12-27 18:48:10 +0200

    more tests for sys_mkdir

--- tests/t006-mkdir.sh
+++ tests/t006-mkdir.sh
@@ -12,6 +12,8 @@ test_expect_success setup '
 '
 
 test_expect_success SYMLINKS setup-symlinks '
+    ln -sf dir4 symlink-dangling-dir4 &&
+    ln -sf dir5 symlink-dangling-dir5
 '
 
 test_expect_success 'deny mkdir()' '
@@ -50,6 +52,151 @@ test_expect_code ATTACH 128 'attach & deny mkdir() for existant directory' '
         $TEST_DIRECTORY_ABSOLUTE/t006_mkdir dir3
     ) &
     pandora -m core/sandbox/path:1 -p $!
+'
+
+test_expect_success SYMLINKS 'deny mkdir() for dangling symlink' '
+    pandora \
+        -EPANDORA_TEST_EPERM=1 \
+        -m core/sandbox/path:1 \
+        -- $TEST_DIRECTORY_ABSOLUTE/t006_mkdir symlink-dangling-dir4
+    test $? = 128 &&
+    test ! -d dir4
+'
+
+test_expect_success ATTACH,SYMLINKS 'attach & deny mkdir() for dangling symlink' '
+    (
+        PANDORA_TEST_EPERM=1
+        export PANDORA_TEST_EPERM
+        sleep 1
+        $TEST_DIRECTORY_ABSOLUTE/t006_mkdir symlink-dangling-dir5
+    ) &
+    pandora -m core/sandbox/path:1 -p $!
+    test $? = 128 &&
+    test ! -d dir5
+'
+
+# FIXME: Why doesn't this work outside of a subshell?
+test_expect_success MKTEMP 'deny mkdir() for existant directory outside' '
+    (
+        d="$(mkstemp --dry-run)"
+        test -n "$d" &&
+        pandora \
+            -EPANDORA_TEST_EPERM=1 \
+            -m core/sandbox/path:1 \
+            -m "allow/path:$HOME_ABSOLUTE/**" \
+            -- $TEST_DIRECTORY_ABSOLUTE/t006_mkdir "$d"
+        test $? = 128 &&
+        test ! -d "$d"
+    ) || return 1
+'
+
+test_expect_success ATTACH,MKTEMP,TODO 'attach & deny mkdir() for existant directory outside' '
+'
+
+# FIXME: Why doesn't this work outside of a subshell?
+test_expect_success MKTEMP,SYMLINKS 'deny mkdir() for symlink outside' '
+    (
+        d="$(mkstemp -d)"
+        test -d "$d" &&
+        ln -sf "$d" symlink0-outside &&
+        pandora \
+            -EPANDORA_TEST_EEXIST=1 \
+            -m core/sandbox/path:1 \
+            -m "allow/path:$HOME_ABSOLUTE/**" \
+            -- $TEST_DIRECTORY_ABSOLUTE/t006_mkdir symlink0-outside
+        test $? = 128
+    ) || return 1
+'
+
+test_expect_code ATTACH,MKTEMP,SYMLINKS 128 'attach & deny mkdir() for symlink outside' '
+    (
+        PANDORA_TEST_EEXIST=1
+        export PANDORA_TEST_EEXIST
+        sleep 1
+        $TEST_DIRECTORY_ABSOLUTE/t006_mkdir symlink1-outside
+    ) &
+    pid=$!
+    d="$(mkstemp -d)"
+    test -d "$d" &&
+    ln -sf "$d" symlink1-outside &&
+    pandora \
+        -m core/sandbox/path:1 \
+        -m "allow/path:$HOME_ABSOLUTE/**" \
+        -p $!
+'
+
+# FIXME: Why doesn't this work outside of a subshell?
+test_expect_success MKTEMP,SYMLINKS 'deny mkdir() for dangling symlink outside' '
+    (
+        d="$(mkstemp --dry-run)"
+        test -n "$d" &&
+        ln -sf "$d" symlink3-outside &&
+        pandora \
+            -EPANDORA_TEST_EPERM=1 \
+            -m core/sandbox/path:1 \
+            -m "allow/path:$HOME_ABSOLUTE/**" \
+            -- $TEST_DIRECTORY_ABSOLUTE/t006_mkdir symlink3-outside
+        test $? = 128 &&
+        test ! -d "$d"
+    ) || return 1
+'
+
+test_expect_success ATTACH,MKTEMP,SYMLINKS 'attach & deny mkdir() dangling for symlink outside' '
+    (
+        PANDORA_TEST_EPERM=1
+        export PANDORA_TEST_EPERM
+        sleep 1
+        $TEST_DIRECTORY_ABSOLUTE/t006_mkdir symlink4-outside
+    ) &
+    pid=$!
+    d="$(mkstemp --dry-run)"
+    test -n "$d" &&
+    ln -sf "$d" symlink4-outside &&
+    pandora \
+        -m core/sandbox/path:1 \
+        -m "allow/path:$HOME_ABSOLUTE/**" \
+        -p $!
+    test $? = 128 &&
+    test ! -d "$d"
+'
+
+test_expect_success 'allow mkdir()' '
+    pandora \
+        -EPANDORA_TEST_SUCCESS=1 \
+        -m core/sandbox/path:1 \
+        -m "allow/path:$HOME_ABSOLUTE/**" \
+        -- $TEST_DIRECTORY_ABSOLUTE/t006_mkdir dir6-non-existant &&
+    test -d dir6-non-existant
+'
+
+test_expect_success ATTACH 'attach & allow mkdir()' '
+    (
+        PANDORA_TEST_SUCCESS=1
+        export PANDORA_TEST_SUCCESS
+        sleep 1
+        $TEST_DIRECTORY_ABSOLUTE/t006_mkdir dir7-non-existant
+    ) &
+    pandora \
+        -m core/sandbox/path:1 \
+        -m "allow/path:$HOME_ABSOLUTE/**" \
+        -p $! &&
+    test -d dir7-non-existant
+'
+
+test_expect_success MKTEMP 'allow mkdir() for non-existant directory outside' '
+    (
+        d="$(mkstemp --dry-run)"
+        test -n "$d" &&
+        pandora \
+            -EPANDORA_TEST_SUCCESS=1 \
+            -m core/sandbox/path:1 \
+            -m "allow/path:$TEMPORARY_DIRECTORY/**" \
+            -- $TEST_DIRECTORY_ABSOLUTE/t006_mkdir "$d" &&
+        test -d "$d"
+    ) || return 1
+'
+
+test_expect_success MKTEMP,TODO 'attach & allow mkdir() for non-existant directory outside' '
 '
 
 test_done
