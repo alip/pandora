d6164b74286e828dc9fbb0644e8bb8cec434f92e
Ali Polatel <alip@exherbo.org>
2010-12-27 12:45:18 +0200

    start adding mkdir tests

--- tests/t006-mkdir.sh
+++ tests/t006-mkdir.sh
@@ -6,4 +6,50 @@
 test_description='sandbox mkdir(2)'
 . ./test-lib.sh
 
+test_expect_success setup '
+    mkdir dir2 &&
+    mkdir dir3
+'
+
+test_expect_success SYMLINKS setup-symlinks '
+'
+
+test_expect_success 'deny mkdir()' '
+    pandora \
+        -EPANDORA_TEST_EPERM=1 \
+        -m core/sandbox/path:1 \
+        -- $TEST_DIRECTORY_ABSOLUTE/t006_mkdir dir0-non-existant
+    test $? = 128 &&
+    test ! -d dir0-non-existant
+'
+
+test_expect_success ATTACH 'attach & deny mkdir()' '
+    (
+        PANDORA_TEST_EPERM=1
+        export PANDORA_TEST_EPERM
+        sleep 1
+        $TEST_DIRECTORY_ABSOLUTE/t006_mkdir dir1-non-existant
+    ) &
+    pandora -m core/sandbox/path:1 -p $!
+    test $? = 128 &&
+    test ! -d dir1-non-existant
+'
+
+test_expect_code 128 'deny mkdir() for existant directory' '
+    pandora \
+        -EPANDORA_TEST_EEXIST=1 \
+        -m core/sandbox/path:1 \
+        -- $TEST_DIRECTORY_ABSOLUTE/t006_mkdir dir2
+'
+
+test_expect_code ATTACH 128 'attach & deny mkdir() for existant directory' '
+    (
+        PANDORA_TEST_EEXIST=1
+        export PANDORA_TEST_EEXIST
+        sleep 1
+        $TEST_DIRECTORY_ABSOLUTE/t006_mkdir dir3
+    ) &
+    pandora -m core/sandbox/path:1 -p $!
+'
+
 test_done
