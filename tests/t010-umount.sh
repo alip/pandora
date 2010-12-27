84827d86abd4322ea1d9a6adfcaa3d5d49f70d67
Ali Polatel <alip@exherbo.org>
2010-12-28 00:52:39 +0200

    add tests for sys_umount

--- tests/t010-umount.sh
+++ tests/t010-umount.sh
@@ -5,5 +5,121 @@
 
 test_description='sandbox umount(2)'
 . ./test-lib.sh
+prog="$TEST_DIRECTORY_ABSOLUTE"/t010_umount
+
+test_expect_success setup '
+    mkdir mnt0 &&
+    mkdir mnt1 &&
+    mkdir mnt4 &&
+    mkdir mnt5
+'
+
+test_expect_success SYMLINKS setup-symlinks '
+    ln -sf /non/existant/directory symlink-dangling
+    ln -sf mnt4 symlink-mnt4 &&
+    ln -sf mnt5 symlink-mnt5
+'
+
+test_expect_code 128 'deny umount()' '
+    pandora \
+        -EPANDORA_TEST_EPERM=1 \
+        -m core/sandbox/path:1 \
+        -- $prog mnt0
+'
+
+test_expect_code ATTACH 128 'attach & deny umount()' '
+    (
+        PANDORA_TEST_EPERM=1
+        export PANDORA_TEST_EPERM
+        sleep 1
+        $prog mnt1
+    ) &
+    pandora -m core/sandbox/path:1 -p $!
+'
+
+test_expect_code 128 'deny umount() for non-existant directory' '
+    pandora \
+        -EPANDORA_TEST_ENOENT=1 \
+        -m core/sandbox/path:1 \
+        -- $prog mnt2-non-existant
+'
+
+test_expect_code ATTACH 128 'attach & deny umount() for non-existant directory' '
+    (
+        PANDORA_TEST_ENOENT=1
+        export PANDORA_TEST_ENOENT
+        sleep 1
+        $prog mnt3-non-existant
+    ) &
+    pandora -m core/sandbox/path:1 -p $!
+'
+
+test_expect_code SYMLINKS 128 'deny umount() for symbolic link' '
+    pandora \
+        -EPANDORA_TEST_EPERM=1 \
+        -m core/sandbox/path:1 \
+        -- $prog symlink-mnt4
+'
+
+test_expect_code ATTACH,SYMLINKS 128 'attach & deny umount() for symbolic link' '
+    (
+        PANDORA_TEST_EPERM=1
+        export PANDORA_TEST_EPERM
+        sleep 1
+        $prog symlink-mnt5
+    ) &
+    pandora \
+        -m core/sandbox/path:1 \
+        -p $!
+'
+
+## FIXME: Why doesn't this work outside of a subshell?
+test_expect_success MKTEMP,SYMLINKS 'deny umount() for symbolic link outside' '
+    (
+        d="$(mkstemp -d)"
+        test -d "$d" &&
+        ln -sf "$d" symlink0-outside &&
+        pandora \
+            -EPANDORA_TEST_EPERM=1 \
+            -m core/sandbox/path:1 \
+            -m "allow/path:$HOME_ABSOLUTE/**" \
+            -- $prog symlink0-outside
+        test $? = 128
+    ) || return 1
+'
+
+test_expect_code ATTACH,MKTEMP,SYMLINKS 128 'attach & deny umount() for symbolic link outside' '
+    (
+        PANDORA_TEST_EPERM=1
+        export PANDORA_TEST_EPERM
+        sleep 1
+        $prog symlink1-outside
+    ) &
+    pid=$!
+    d="$(mkstemp -d)"
+    test -d "$d" &&
+    ln -sf "$d" symlink1-outside &&
+    pandora \
+        -m core/sandbox/path:1 \
+        -m "allow/path:$HOME_ABSOLUTE/**" \
+        -p $!
+'
+
+test_expect_code SYMLINKS 128 'deny umount() for dangling symbolic link' '
+    pandora \
+        -EPANDORA_TEST_ENOENT=1 \
+        -m core/sandbox/path:1 \
+        -- $prog symlink-dangling
+'
+
+test_expect_code ATTACH,SYMLINKS 128 'attach & deny umount() for dangling symbolic link' '
+    (
+        PANDORA_TEST_ENOENT=1
+        export PANDORA_TEST_ENOENT
+        sleep 1
+        $prog symlink-dangling
+    ) &
+    pandora -m core/sandbox/path:1 -p $!
+'
 
 test_done
---
+++ tests/t010_umount.c
@@ -0,0 +1,29 @@
+/* vim: set cino= fo=croql sw=8 ts=8 sts=0 noet cin fdm=syntax : */
+
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char **argv)
+{
+	if (argc < 1)
+		return 125;
+
+	if (umount(argv[1]) < 0) {
+		if (getenv("PANDORA_TEST_SUCCESS")) {
+			perror(__FILE__);
+			return 1;
+		}
+		else if (getenv("PANDORA_TEST_EPERM") && errno == EPERM)
+			return 0;
+		else if (getenv("PANDORA_TEST_ENOENT") && errno == ENOENT)
+			return 0;
+		perror(__FILE__);
+		return 1;
+	}
+
+	return getenv("PANDORA_TEST_SUCCESS") ? 0 : 2;
+}
